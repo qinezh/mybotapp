a7ddbbe231665e2a4e09bcb7cc4deaaeb2175bd8
dooriya <dol@microsoft.com>
2022-02-25 12:24:59 +0800

    add support for adaptive card

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,21 +1,34 @@
-import { Activity, CardFactory } from "botbuilder";
-import { AdaptiveCards } from "@microsoft/adaptivecards-tools";
+import { Activity } from "botbuilder";
 import * as cron from "node-cron";
 import { TeamsFxBot } from "./sdk/bot";
 import { adapter } from "./adapter";
 import { server } from "./server";
-import messageTemplate from "./message.template.json";
+import { buildBotMessage, CardData } from "./message";
+
+/**
+ * This method is customized by userd to retrirve the notification data and transform to the CardData model.
+ * @returns a card data object
+ */
+function getCardData(): CardData {
+  // This is just to mock the notification data retrieval
+  const rawData = {
+    title: "New Event Occurred!", 
+    description: "Detailed description of what happened so the user knows what's going on.",
+    url : "https://www.adaptivecards.io/",
+  };
+
+  // A default transformer to transform the original notification data to card data model.
+  return {
+    title: rawData.title,
+    appName: "Contoso App Notification",
+    description: rawData.description,
+    notificationUrl: rawData.url
+  }
+}
+
+const message: Partial<Activity> = buildBotMessage(getCardData);
 
 const teamsfxBot = new TeamsFxBot(adapter);
-
-const message: Partial<Activity> = {
-  attachments: [
-    CardFactory.adaptiveCard(AdaptiveCards.declare(messageTemplate).render({
-      title: "Notification Test",
-      message: "This is a notification from TeamsFx bot."
-    }))
-  ]
-};
 
 // HTTP trigger to send notification.
 server.post("/api/notify/default", async (req, res) => {
---
+++ bot/src/message.ts
@@ -0,0 +1,30 @@
+import { Activity, CardFactory } from "botbuilder";
+import { AdaptiveCards } from "@microsoft/adaptivecards-tools";
+import messageTemplate from "./message.template.json";
+
+/**
+ * Adaptive card data model
+ */
+export interface CardData {
+  title: string,
+  appName: string,
+  description: string,
+  notificationUrl: string
+}
+
+/**
+ * Utility method to comvert the message data to adaptive card
+ */
+export function buildBotMessage(getCardData: () => CardData): Partial<Activity> {
+  // Get notification raw data
+  const cardData = getCardData();
+
+  // Wrap the message in adaptive card
+  return {
+    attachments: [
+      CardFactory.adaptiveCard(
+        AdaptiveCards.declare<CardData>(messageTemplate).render(cardData)
+      )
+    ]
+  };
+}
